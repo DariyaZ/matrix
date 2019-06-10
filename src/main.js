cdd0c66d0b35f0f728e0222f2503ff3716a50c50
Dariia Zhyr <darzhyr@gmail.com>
2019-06-10 15:40:06 +0300

    Fixed iteration

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
 
 const matrices = document.getElementsByClassName('flex_item');
 
-let j = 1;
+let j = 0;
 let k = j;
 for (let i = 0; i < matrices.length; i++) {
-  for (j = 1; j < matrices[i].children.length + 1; j++) {
-    matrices[i].children[j - 1].innerHTML = getFibonacciNumber(k);
+  for (j = 0; j < matrices[i].children.length; j++) {
+    matrices[i].children[j].innerHTML = getFibonacciNumber(k);
     k++;
   }
 }
